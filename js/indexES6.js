497fdec22b8fd5eb21548c1eb31424cb52f48f43
williamsimms <williamsimms193@gmail.com>
2020-07-22 14:41:45 -0400

    es6 classes

--- js/indexES6.js
+++ js/indexES6.js
@@ -5,16 +5,82 @@ const isbn = document.getElementById('isbn')
 const list = document.getElementById('book-list')
 const container = document.querySelector('.container')
 
-
 class Book {
-    constructor(book, author, title) {
-        this.book = book
-        this.author = author
+    constructor(title, author, isbn) {
         this.title = title
+        this.author = author
+        this.isbn = isbn
     }
 }
 
-
 class UI {
+    addBookToList(book) {
+        const row = document.createElement('tr')
+        row.innerHTML = `
+     <td>${book.title}</td>
+     <td>${book.author}</td>
+     <td>${book.isbn}</td>
+     <td><a href = '#' class = 'delete'>X </a></td>
+     `
+        list.appendChild(row)
+    }
 
+    clearFields() {
+        title.value = ''
+        author.value = ''
+        isbn.value = ''
+    }
+
+    deleteBook(target) {
+        if (target.classList.contains('delete')) {
+            target.parentElement.parentElement.remove()
+        }
+    }
+
+    showAlert(message, className) {
+        const div = document.createElement('div')
+        div.className = `alert ${className}`
+        div.appendChild(document.createTextNode(message))
+        //Insert Alert
+        container.insertBefore(div, form)
+
+        // Timeout after 3 seconds
+        setTimeout(() => {
+            const alert = document.querySelector('.alert')
+            alert.remove()
+        }, 3000)
+    }
 }
+
+//Event Listener For Adding a new Book
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    //Getting Form Values
+    const titleValue = title.value
+    const authorValue = author.value
+    const isbnValue = isbn.value
+
+    // Instantiating book constructor
+    const book = new Book(titleValue, authorValue, isbnValue)
+
+    // Instantiating UI Constructor 
+
+    const ui = new UI()
+
+    if (titleValue === '' || authorValue === '' || isbnValue === '') {
+        ui.showAlert('Please Fill All Fields', 'error')
+    } else {
+        ui.addBookToList(book)
+        ui.clearFields()
+        ui.showAlert('Book Added', 'success')
+    }
+
+})
+
+// Event Listener For Deleting a Book
+list.addEventListener('click', (e) => {
+    const ui = new UI()
+    // Delete Book
+    ui.deleteBook(e.target)
+    ui.showAlert('Book Deleted', 'success')
+})
